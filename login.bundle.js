d53b7efcf5535a0de51d21a4178e3bf416455635
rintheo <107115767+rintheo@users.noreply.github.com>
2024-01-04 22:32:42 +0800

    Initial UI for login screen

---
+++ login.bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/login.js ***!
+  \**********************/
+const buttonLogin = document.querySelector('#buttonLogin');
+const buttonSignup = document.querySelector('#buttonSignup');
+const buttonExistingAccount = document.querySelector('#buttonExistingAccount');
+const buttonNewAccount = document.querySelector('#buttonNewAccount');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibG9naW4uYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLWxpc3QvLi9zcmMvbG9naW4uanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgYnV0dG9uTG9naW4gPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcjYnV0dG9uTG9naW4nKTtcclxuY29uc3QgYnV0dG9uU2lnbnVwID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcignI2J1dHRvblNpZ251cCcpO1xyXG5jb25zdCBidXR0b25FeGlzdGluZ0FjY291bnQgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcjYnV0dG9uRXhpc3RpbmdBY2NvdW50Jyk7XHJcbmNvbnN0IGJ1dHRvbk5ld0FjY291bnQgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcjYnV0dG9uTmV3QWNjb3VudCcpO1xyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
